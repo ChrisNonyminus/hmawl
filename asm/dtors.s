71fbff3ab47ac195d54c131c6d4304a8dd35db59
Christian <50080567+ChrisNonyminus@users.noreply.github.com>
2021-12-09 17:58:37 -0700

    nuke incbins

--- asm/dtors.s
+++ asm/dtors.s
@@ -3,4 +3,9 @@
 .section .dtors, "wa"  # 0x8023DF00 - 0x8023DF20
 .global lbl_8023DF00
 lbl_8023DF00:
-	.incbin "baserom.dol", 0x23AF00, 0x10
+	# ROM: 0x23AF00
+	.4byte 0x8023590C  ;# ptr
+	.4byte 0x802364E0  ;# ptr
+	.4byte 0x8023590C  ;# ptr
+	.4byte 0
+
