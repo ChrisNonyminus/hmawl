24ffe8d44791eac1f30d029b46328a58f6d6ec97
Christian <50080567+ChrisNonyminus@users.noreply.github.com>
2022-08-09 16:00:33 -0600

    (temp) add FAILING rel disassemblies
    Gonna use the list of errors to kill more pointers though

---
+++ asm/rels/wlp2.s
@@ -0,0 +1,233 @@
+# id: 3
+# version: 3
+# nameoffset: 0x46, size: 0x23
+# section table: 0x4C, size: 0xA8
+# imp table: 0x2B3BC
+# relocs offset: 0x2B3CC
+# _prolog:     1:0x21778
+# _epilog:     1:0x217C8
+# _unresolved: 1:0x21814
+# num sections: 21
+.include "macros.inc"
+# offset: 0x00000000	length: 0x00000000	flags: 0
+# offset: 0x000000F4	length: 0x00021880	flags: 1
+# offset: 0x00021974	length: 0x00000028	flags: 0
+# offset: 0x0002199C	length: 0x00000008	flags: 0
+# offset: 0x000219C0	length: 0x00001BC4	flags: 0
+# offset: 0x000235A0	length: 0x00007E1B	flags: 0
+# offset: 0x10000000	length: 0x00000304	flags: 0
+# offset: 0x00000000	length: 0x00000000	flags: 0
+# offset: 0x00000000	length: 0x00000000	flags: 0
+# offset: 0x00000000	length: 0x00000000	flags: 0
+# offset: 0x00000000	length: 0x00000000	flags: 0
+# offset: 0x00000000	length: 0x00000000	flags: 0
+# offset: 0x00000000	length: 0x00000000	flags: 0
+# offset: 0x00000000	length: 0x00000000	flags: 0
+# offset: 0x00000000	length: 0x00000000	flags: 0
+# offset: 0x00000000	length: 0x00000000	flags: 0
+# offset: 0x00000000	length: 0x00000000	flags: 0
+# offset: 0x00000000	length: 0x00000000	flags: 0
+# offset: 0x00000000	length: 0x00000000	flags: 0
+# offset: 0x00000000	length: 0x00000000	flags: 0
+# offset: 0x00000000	length: 0x00000000	flags: 0
+error: symbol for 0x8001BFEC not found
+error: symbol for 0x8015CF18 not found
+error: symbol for 0x8015DA54 not found
+error: symbol for 0x8015CF18 not found
+error: symbol for 0x80027A4C not found
+error: symbol for 0x8007E7B0 not found
+error: symbol for 0x80136260 not found
+error: symbol for 0x8007E96C not found
+error: symbol for 0x8001C0D0 not found
+error: symbol for 0x801A8B34 not found
+error: symbol for 0x801A8B34 not found
+error: symbol for 0x80027564 not found
+error: symbol for 0x801A8B34 not found
+error: symbol for 0x801A8B34 not found
+error: symbol for 0x80349168 not found
+error: symbol for 0x80349168 not found
+error: symbol for 0x80349168 not found
+error: symbol for 0x80349168 not found
+error: symbol for 0x8002283C not found
+error: symbol for 0x800238C4 not found
+error: symbol for 0x800238CC not found
+error: symbol for 0x801A37B4 not found
+error: symbol for 0x8001CA8C not found
+error: symbol for 0x8001CA68 not found
+error: symbol for 0x8001CA68 not found
+error: symbol for 0x8001CAB0 not found
+error: symbol for 0x801A3824 not found
+error: symbol for 0x80027C00 not found
+error: symbol for 0x80027C00 not found
+error: symbol for 0x80027C00 not found
+error: symbol for 0x80027C00 not found
+error: symbol for 0x80130208 not found
+error: symbol for 0x801A8A0C not found
+error: symbol for 0x801A8A0C not found
+error: symbol for 0x801A8A0C not found
+error: symbol for 0x801A8A0C not found
+error: symbol for 0x801A8A0C not found
+error: symbol for 0x801A8A0C not found
+error: symbol for 0x801A8A0C not found
+error: symbol for 0x801A8A0C not found
+error: symbol for 0x801A8A0C not found
+error: symbol for 0x801A8A0C not found
+error: symbol for 0x801A8A0C not found
+error: symbol for 0x801A8A0C not found
+error: symbol for 0x801A892C not found
+error: symbol for 0x801A892C not found
+error: symbol for 0x800D68B0 not found
+error: symbol for 0x800D68B0 not found
+error: symbol for 0x801A892C not found
+error: symbol for 0x801A892C not found
+error: symbol for 0x801A892C not found
+error: symbol for 0x801A892C not found
+error: symbol for 0x801A8A0C not found
+error: symbol for 0x801A8A0C not found
+error: symbol for 0x801A8A0C not found
+error: symbol for 0x801A8A0C not found
+error: symbol for 0x801A892C not found
+error: symbol for 0x801A892C not found
+error: symbol for 0x800D68B0 not found
+error: symbol for 0x801A8A0C not found
+error: symbol for 0x801A892C not found
+error: symbol for 0x801A892C not found
+error: symbol for 0x801A892C not found
+error: symbol for 0x801A892C not found
+error: symbol for 0x801A8B10 not found
+error: symbol for 0x801A8A74 not found
+error: symbol for 0x801A8A8C not found
+error: symbol for 0x801A8A8C not found
+error: symbol for 0x801A8A8C not found
+error: symbol for 0x801A8AA4 not found
+error: symbol for 0x801A8AD8 not found
+error: symbol for 0x801A8AA4 not found
+error: symbol for 0x801A8AD8 not found
+error: symbol for 0x801A8AA4 not found
+error: symbol for 0x801A8AD8 not found
+error: symbol for 0x801A8AA4 not found
+error: symbol for 0x801A8AD8 not found
+error: symbol for 0x801A8AD8 not found
+error: symbol for 0x801A8AD8 not found
+error: symbol for 0x801A8AD8 not found
+error: symbol for 0x801A8AD8 not found
+error: symbol for 0x801A8AD8 not found
+error: symbol for 0x801A8AD8 not found
+error: symbol for 0x801A8AD8 not found
+error: symbol for 0x801A8AD8 not found
+error: symbol for 0x801A89B8 not found
+error: symbol for 0x801A8A0C not found
+error: symbol for 0x800FCAB4 not found
+error: symbol for 0x800D6830 not found
+error: symbol for 0x800D6830 not found
+error: symbol for 0x801A8940 not found
+error: symbol for 0x801A8940 not found
+error: symbol for 0x801A8940 not found
+error: symbol for 0x801A8940 not found
+error: symbol for 0x801A8940 not found
+error: symbol for 0x801A8940 not found
+error: symbol for 0x801A8940 not found
+error: symbol for 0x801A8940 not found
+error: symbol for 0x801A8940 not found
+error: symbol for 0x801A8940 not found
+error: symbol for 0x801A8940 not found
+error: symbol for 0x801A8940 not found
+error: symbol for 0x800D67C0 not found
+error: symbol for 0x8008E8D0 not found
+error: symbol for 0x8008E8D0 not found
+error: symbol for 0x8008E8D0 not found
+error: symbol for 0x8008E8D0 not found
+error: symbol for 0x8008E3D4 not found
+error: symbol for 0x8008E3D4 not found
+error: symbol for 0x8008E8EC not found
+error: symbol for 0x8008E8EC not found
+error: symbol for 0x8008E8EC not found
+error: symbol for 0x8008E754 not found
+error: symbol for 0x8008E790 not found
+error: symbol for 0x8008E8D0 not found
+error: symbol for 0x8008E8D0 not found
+error: symbol for 0x8017EB1C not found
+error: symbol for 0x8017EC2C not found
+error: symbol for 0x8017EA34 not found
+error: symbol for 0x8017EC2C not found
+error: symbol for 0x8002283C not found
+error: symbol for 0x800229E0 not found
+error: symbol for 0x80022A20 not found
+error: symbol for 0x80022A60 not found
+error: symbol for 0x80027644 not found
+error: symbol for 0x800887D8 not found
+error: symbol for 0x800887D8 not found
+error: symbol for 0x800275FC not found
+error: symbol for 0x800581F4 not found
+error: symbol for 0x80088F58 not found
+error: symbol for 0x800887C8 not found
+error: symbol for 0x800887D8 not found
+error: symbol for 0x800887C8 not found
+error: symbol for 0x800887C8 not found
+error: symbol for 0x80054354 not found
+error: symbol for 0x80054354 not found
+error: symbol for 0x80152040 not found
+error: symbol for 0x80145324 not found
+error: symbol for 0x800FD598 not found
+error: symbol for 0x8004C098 not found
+error: symbol for 0x8004C254 not found
+error: symbol for 0x800FD598 not found
+error: symbol for 0x8014541C not found
+error: symbol for 0x80145374 not found
+error: symbol for 0x8014541C not found
+error: symbol for 0x80145424 not found
+error: symbol for 0x8014641C not found
+error: symbol for 0x80146458 not found
+error: symbol for 0x800581C4 not found
+error: symbol for 0x80053DE8 not found
+error: symbol for 0x80053DE8 not found
+error: symbol for 0x80053E0C not found
+error: symbol for 0x80053DD8 not found
+error: symbol for 0x800FD598 not found
+error: symbol for 0x80053DE8 not found
+error: symbol for 0x80053DE8 not found
+error: symbol for 0x80053DE8 not found
+error: symbol for 0x80053DE8 not found
+error: symbol for 0x80053E0C not found
+error: symbol for 0x80053DD8 not found
+error: symbol for 0x80053DE8 not found
+error: symbol for 0x8034AF5C not found
+error: symbol for 0x8034AF5C not found
+error: symbol for 0x8034AF60 not found
+error: symbol for 0x8034AF60 not found
+error: symbol for 0x80053DE8 not found
+error: symbol for 0x80053DE8 not found
+error: symbol for 0x80053E0C not found
+error: symbol for 0x80053DE8 not found
+error: symbol for 0x80053DE8 not found
+error: symbol for 0x80053DE8 not found
+error: symbol for 0x80146458 not found
+error: symbol for 0x8014641C not found
+error: symbol for 0x80058178 not found
+error: symbol for 0x80088EB0 not found
+error: symbol for 0x80088834 not found
+error: symbol for 0x800FD598 not found
+error: symbol for 0x800E5604 not found
+error: symbol for 0x800E5638 not found
+error: symbol for 0x800E5670 not found
+error: symbol for 0x80191448 not found
+error: symbol for 0x800E56AC not found
+error: symbol for 0x800E5854 not found
+error: symbol for 0x8018D16C not found
+error: symbol for 0x8018B528 not found
+error: symbol for 0x8018B69C not found
+error: symbol for 0x8018D41C not found
+error: symbol for 0x800EC4D4 not found
+error: symbol for 0x80015AAC not found
+error: symbol for 0x8008DC28 not found
+error: symbol for 0x800F1414 not found
+error: symbol for 0x8018D16C not found
+error: symbol for 0x8018B57C not found
+error: symbol for 0x80015A94 not found
+error: symbol for 0x8008E0A8 not found
+error: symbol for 0x8008E0F4 not found
+error: symbol for 0x8008E200 not found
+error: symbol for 0x8008E298 not found
+error: symbol for 0x800E52CC not found
+error: symbol for 0x801601A0 not found
+error: symbol for 0x801601A0 not found
