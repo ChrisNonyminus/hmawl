85a5f8075661c7bbdf8a1aa9d6b1edb7e67c8e4b
Christian <50080567+ChrisNonyminus@users.noreply.github.com>
2022-08-09 06:57:19 -0600

    label extab

--- asm/extab.s
+++ asm/extab.s
@@ -5,68 +5,132 @@
 lbl_extab:
 	.4byte 0x00080000
 	.4byte 0
+.global lbl_800056C8
+lbl_800056C8:
 	.4byte 0x08080000
 	.4byte 0
+.global lbl_800056D0
+lbl_800056D0:
 	.4byte 0x28080000
 	.4byte 0
+.global lbl_800056D8
+lbl_800056D8:
 	.4byte 0x00080000
 	.4byte 0
+.global lbl_800056E0
+lbl_800056E0:
 	.4byte 0x00080000
 	.4byte 0
+.global lbl_800056E8
+lbl_800056E8:
 	.4byte 0x60080000
 	.4byte 0
+.global lbl_800056F0
+lbl_800056F0:
 	.4byte 0x00080000
 	.4byte 0
+.global lbl_800056F8
+lbl_800056F8:
 	.4byte 0x00080000
 	.4byte 0
+.global lbl_80005700
+lbl_80005700:
 	.4byte 0x108A0000
 	.4byte 0
+.global lbl_80005708
+lbl_80005708:
 	.4byte 0x30480000
 	.4byte 0
+.global lbl_80005710
+lbl_80005710:
 	.4byte 0x080A0000
 	.4byte 0
+.global lbl_80005718
+lbl_80005718:
 	.4byte 0x000A0000
 	.4byte 0
+.global lbl_80005720
+lbl_80005720:
 	.4byte 0x000A0000
 	.4byte 0
+.global lbl_80005728
+lbl_80005728:
 	.4byte 0x00080000
 	.4byte 0
+.global lbl_80005730
+lbl_80005730:
 	.4byte 0x00080000
 	.4byte 0
+.global lbl_80005738
+lbl_80005738:
 	.4byte 0x00080000
 	.4byte 0
+.global lbl_80005740
+lbl_80005740:
 	.4byte 0x08080000
 	.4byte 0
+.global lbl_80005748
+lbl_80005748:
 	.4byte 0x00080000
 	.4byte 0
+.global lbl_80005750
+lbl_80005750:
 	.4byte 0x20080000
 	.4byte 0
+.global lbl_80005758
+lbl_80005758:
 	.4byte 0x08080000
 	.4byte 0
+.global lbl_80005760
+lbl_80005760:
 	.4byte 0x00080000
 	.4byte 0
+.global lbl_80005768
+lbl_80005768:
 	.4byte 0x00080000
 	.4byte 0
+.global lbl_80005770
+lbl_80005770:
 	.4byte 0x10080000
 	.4byte 0
+.global lbl_80005778
+lbl_80005778:
 	.4byte 0x20080000
 	.4byte 0
+.global lbl_80005780
+lbl_80005780:
 	.4byte 0x28080000
 	.4byte 0
+.global lbl_80005788
+lbl_80005788:
 	.4byte 0x08080000
 	.4byte 0
+.global lbl_80005790
+lbl_80005790:
 	.4byte 0x30080000
 	.4byte 0
+.global lbl_80005798
+lbl_80005798:
 	.4byte 0x20080000
 	.4byte 0
+.global lbl_800057A0
+lbl_800057A0:
 	.4byte 0x20080000
 	.4byte 0x00000060
+.global lbl_800057A8
+lbl_800057A8:
 	.4byte 0x00000010
 	.4byte 0
+.global lbl_800057B0
+lbl_800057B0:
 	.4byte 0x82000008
 	.4byte lbl_80235B88
+.global lbl_800057B8
+lbl_800057B8:
 	.4byte 0x18080000
 	.4byte 0
+.global lbl_800057C0
+lbl_800057C0:
 	.4byte 0x28080000
 	.4byte 0x00000070
 	.4byte 0x00000010
