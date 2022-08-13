d966387f124a0f2d0c5908f06a13ab757e004c57
Christian <50080567+ChrisNonyminus@users.noreply.github.com>
2022-08-13 07:37:31 -0600

    split out PPCArch

--- obj_files.mk
+++ obj_files.mk
@@ -3,6 +3,7 @@ EXTAB_O_FILES := $(BUILD_DIR)/asm/extab_.o
 EXTABINDEX_O_FILES := $(BUILD_DIR)/asm/extabindex_.o
 TEXT_O_FILES := $(BUILD_DIR)/asm/main/code_80005980.o\
 				$(BUILD_DIR)/asm/main/code_80007114.o\
+				$(BUILD_DIR)/asm/main/PPCArch.o\
 				$(BUILD_DIR)/asm/main/code_801B0A18.o
 CTORS_O_FILES := $(BUILD_DIR)/asm/ctors.o
 DTORS_O_FILES := $(BUILD_DIR)/asm/dtors.o
