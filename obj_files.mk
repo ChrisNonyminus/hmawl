62d9fea6d13cd9afc8d9a34712068fa62c3f6e4f
Christian <50080567+ChrisNonyminus@users.noreply.github.com>
2022-08-13 14:12:30 -0600

    hsd: split dobj and tobj

--- obj_files.mk
+++ obj_files.mk
@@ -3,9 +3,15 @@ EXTAB_O_FILES := $(BUILD_DIR)/asm/extab_.o
 EXTABINDEX_O_FILES := $(BUILD_DIR)/asm/extabindex_.o
 TEXT_O_FILES := $(BUILD_DIR)/asm/main/code_80005980.o\
 				$(BUILD_DIR)/asm/main/code_80007114.o\
+				\
 				$(BUILD_DIR)/asm/main/libraries/base/PPCArch.o\
+				\
 				$(BUILD_DIR)/asm/main/libraries/dolphin.o\
+				\
+				$(BUILD_DIR)/asm/main/libraries/sysdolphin/dobj.o\
+				$(BUILD_DIR)/asm/main/libraries/sysdolphin/tobj.o\
 				$(BUILD_DIR)/asm/main/libraries/sysdolphin.o\
+				\
 				$(BUILD_DIR)/asm/main/code_801B0A18.o
 CTORS_O_FILES := $(BUILD_DIR)/asm/ctors.o
 DTORS_O_FILES := $(BUILD_DIR)/asm/dtors.o
