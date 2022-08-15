60d4dabc956353af8a958364cfd63cdf2f9ebcd0
Christian <50080567+ChrisNonyminus@users.noreply.github.com>
2022-08-15 16:01:02 -0600

    Dvd.s

--- obj_files.mk
+++ obj_files.mk
@@ -9,7 +9,8 @@ TEXT_O_FILES := $(BUILD_DIR)/asm/main/game/code_80005980.o\
 				$(BUILD_DIR)/src/game/Memory.o\
 				$(BUILD_DIR)/asm/main/game/Memory.o\
 				$(BUILD_DIR)/asm/main/game/code_80179F18.o\
-				$(BUILD_DIR)/asm/main/game/code_8017AB2C.o\
+				$(BUILD_DIR)/asm/main/game/Dvd.o\
+				$(BUILD_DIR)/asm/main/game/code_8017B5F0.o\
 				$(BUILD_DIR)/asm/main/game/CKernel.o\
 				$(BUILD_DIR)/asm/main/game/code_80182578.o\
 				\
